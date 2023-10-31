4208a575311dc59cbfe3c17bbc3c157e2d3848a3
Thanvi Amara <69161504+amarathanvi@users.noreply.github.com>
2023-10-31 04:59:04 -0400

    Delete Amara_Thanvendraeswar_hw3_secondPhaseOfApp directory

--- Amara_Thanvendraeswar_hw3_secondPhaseOfApp/js/test.js
+++
@@ -1,5 +0,0 @@
-function ShowHelloMessage() {
-    var name = document.getElementById("firstName");
-    document.getElementById("hellomessage").innerHTML = "Hello, " + name.value + ". Thanks for your feedback";
-}
-document.getElementById("submit").onclick = ShowHelloMessage;
